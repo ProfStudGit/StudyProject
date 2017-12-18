54407fc313c2f2bfabdf0bd7d54cb398e1d87963
ProfStudGit <34652784+ProfStudGit@users.noreply.github.com>
2017-12-18 15:05:39 +0100

    fasd

---
+++ Assets/Finalizado/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+
+    [SerializeField]
+    private Vector3 m_Offset;
+
+    [SerializeField]
+    private float m_MoveSpeed;
+
+    private void Update()
+    {
+        //Get the targets position.
+        Vector3 pos = m_Target.localPosition;
+
+        //Add the offset.
+        Vector3 posWithOffset = pos;
+        posWithOffset += m_Offset;
+
+        Vector3 worldPosWithOffset = m_Target.localToWorldMatrix.MultiplyPoint(posWithOffset);
+
+        //Move this object towards that position.
+        transform.position = Vector3.Lerp(transform.position, worldPosWithOffset, Time.deltaTime * m_MoveSpeed);
+
+        //Rotate this object to face the target.
+        transform.rotation = Quaternion.Slerp(transform.rotation, m_Target.rotation, Time.deltaTime * m_MoveSpeed);
+    }
+}
